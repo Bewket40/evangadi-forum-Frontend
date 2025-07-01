a0333525e0faabd47e04955bc84a569c0d23232a
Bewket Wondim <bwondim2014@gmail.com>
2025-06-30 21:33:14 -0400

    built package

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.css"
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+// import App from './App.jsx'
+
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
